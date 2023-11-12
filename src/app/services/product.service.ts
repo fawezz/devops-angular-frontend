efbf8de4b50da36bbaa08e173d4b3bae2f0b04af
faouez <fmarzoukk@gmail.com>
2023-11-12 15:14:41 +0100

    base url context path

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://192.168.33.10:8082'; 
+  private apiUrl = 'http://192.168.33.10:8082/SpringMVC'; 
 
   constructor(private http: HttpClient) { }
 
